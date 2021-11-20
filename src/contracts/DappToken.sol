141a6e0a7f5952820b0b516e3712d1fa771abe7d
Michael Kees <80150652+crownholder@users.noreply.github.com>
2021-11-19 17:45:22 -0800

    Update DappToken.sol

--- src/contracts/DappToken.sol
+++ src/contracts/DappToken.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.6.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract DappToken is ERC20 {
-    constructor() public ERC20("DApp Token", "DAPP") {
-        _mint(msg.sender, 1000000000000000000000000);
+    constructor() public ERC20("ANTIDOTE", "ADOT") {
+        _mint(msg.sender, 9000000000000000000000000);
     }
 }
